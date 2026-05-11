720046142bd3de26d11108ef77afb6c9ea3e6765
Shreyansh Sharma <sharmashreyansh001@gmail.com>
2026-05-11 21:01:34 +0530

    size increase logo

--- src/app/page.js
+++ src/app/page.js
@@ -20,8 +20,8 @@ export default function IITJamPrepPlatform() {
             <Image
               src="/logo.png"
               alt="Logo"
-              width={56}
-              height={56}
+              width={100}
+              height={100}
               className="rounded-2xl"
             />
 
